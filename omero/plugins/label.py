cccaadf9de629af518568380a5c48dc136b84543
Sébastien Besson <sbesson@glencoesoftware.com>
2026-07-02 16:20:53 +0100

    Add initial version of utilities for managing OME-Zarr labels in OMERO
    - `omero_label` includes several utilities for creating and querying the
    OMERO objects associated with OME-Zarr labels
    - `omero/plugins` adds an OMERO CLI plugin `omero label` for interacting
    with OME-Zarr labels (register, get, update)

---
+++ omero/plugins/label.py
@@ -0,0 +1,150 @@
+# encoding: utf-8
+#
+# Copyright (c) 2025 Glencoe Software, Inc. All rights reserved.
+#
+# This software is distributed under the terms described by the LICENCE file
+# you can find at the root of the distribution bundle.
+# If the file is missing please request a copy by contacting
+# support@glencoesoftware.com.
+
+
+import sys
+
+from omero.cli import BaseControl
+from omero.cli import CLI
+from omero.model import ExternalInfoI
+from omero.model import ImageI
+from omero.model import RoiI
+from omero.rtypes import rstring
+from omero.rtypes import unwrap
+
+from omero_label import create_mask, create_external_info, \
+                        query_pixels, query_mask
+
+
+HELP = """Register and manage OME-Zarr labels
+
+Examples:
+
+    $ omero label register /data/image.zarr/0/labels/cells 1
+    Roi:5358
+    $ omero label get 5358
+    x=0.0
+    y=0.0
+    width=1584.0
+    height=788.0
+    fillColor=-65436
+    uri=/data/image.zarr/0/labels/cells
+    name=label
+    $ omero label get 5358 uri
+    /data/image.zarr/0/labels/cells
+    $ omero label update 5358 /data/image.zarr/0/labels/cells_new
+"""
+
+NGFF_ENTITY_TYPE = 'com.glencoesoftware.ngff:multiscales'
+NGFF_ENTITY_ID = 3
+
+
+class LabelControl(BaseControl):
+
+    def _configure(self, parser):
+        parser.add_login_arguments()
+        sub = parser.sub()
+
+        register = parser.add(
+            sub, self.register, help="Register an OME-Zarr label")
+        register.add_argument(
+            "uri", type=str, help="URI to the OME-Zarr label")
+        register.add_argument(
+            "image_id", type=int,
+            help="ID of the image to associate the label with")
+        register.add_argument(
+            "--name", type=str, default="label", help="Label name")
+
+        get = parser.add(
+            sub, self.get, help="Retrieve the label properties")
+        get.add_argument(
+            "label_id", type=int,
+            help="ID of the ROI object associated with the label")
+        get.add_argument(
+            "property", type=str, nargs="?",
+            help="Label property to retrieve")
+
+        update = parser.add(
+            sub, self.update, help="Update a label uri")
+        update.add_argument(
+            "label_id", type=int,
+            help="ID of the ROI object associated with the label")
+        update.add_argument(
+            "uri", type=str, help="New URI to the OME-Zarr label")
+
+    def register(self, args):
+        client = self.ctx.conn(args)
+        session = client.getSession()
+
+        pixels = query_pixels(session, args.image_id)
+        if pixels is None:
+            self.ctx.die(334, f"No image {args.image_id} found")
+
+        roi = RoiI()
+        roi.setName(rstring(args.name))
+        mask = create_mask(0, 0, pixels.sizeX.val, pixels.sizeY.val, args.name)
+        mask.details.externalInfo = create_external_info(
+            args.uri,
+            NGFF_ENTITY_TYPE,
+            NGFF_ENTITY_ID)
+        roi.addShape(mask)
+        roi.setImage(ImageI(args.image_id, False))
+
+        roi = session.getUpdateService().saveAndReturnObject(
+            roi, {"omero.group": str(pixels.details.group.id.val)})
+        self.ctx.out(f"Roi:{roi.id.val}")
+
+    def update(self, args):
+        client = self.ctx.conn(args)
+        session = client.getSession()
+        mask = query_mask(session, args.label_id)
+        if mask is None:
+            self.ctx.die(334, f"No ROI {args.roi_id} found")
+
+        old_externalInfo = mask.details.externalInfo
+        mask.details.externalInfo = create_external_info(
+            args.uri,
+            NGFF_ENTITY_TYPE,
+            NGFF_ENTITY_ID)
+
+        session.getUpdateService().saveObject(
+            mask, {"omero.group": str(mask.details.group.id.val)})
+        session.getUpdateService().deleteObject(
+            ExternalInfoI(old_externalInfo.id.val, False))
+
+    def get(self, args):
+        client = self.ctx.conn(args)
+        session = client.getSession()
+        mask = query_mask(session, args.label_id)
+        if mask is None:
+            self.ctx.die(334, f"No ROI {args.roi_id} found")
+
+        properties = {
+            "x": unwrap(mask.x),
+            "y": unwrap(mask.x),
+            "width": unwrap(mask.width),
+            "height": unwrap(mask.height),
+            "fillColor": unwrap(mask.fillColor),
+            "uri": unwrap(mask.details.externalInfo.lsid),
+            "name": unwrap(mask.textValue),
+        }
+        if args.property is not None:
+            self.ctx.out(f"{properties[args.property]}")
+        else:
+            for k, v in properties.items():
+                self.ctx.out(f"{k}={v}")
+
+
+try:
+    register("label", LabelControl, HELP)
+except NameError:
+    if __name__ == "__main__":
+        cli = CLI()
+        cli.register("label", LabelControl, HELP)
+        cli.invoke(sys.argv[1:])
---
+++ omero_label/__init__.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2025 Glencoe Software, Inc.  All rights reserved.
+#
+# This software is distributed under the terms described by the LICENCE.txt
+# file you can find at the root of the distribution bundle.  If the file is
+# missing please request a copy by contacting info@glencoesoftware.com.
+
+import logging
+import omero
+import omero.clients
+
+from omero.rtypes import rdouble, rint, rlong, rstring
+from omero.model import ExternalInfoI
+from omero.model import ImageI
+from omero.model import MaskI
+from omero.model import RoiI
+
+LOGGER = logging.getLogger(__name__)
+
+NGFF_LABEL_ENTITY_TYPE = "com.glencoesoftware.ngff:multiscales"
+NGFF_LABEL_ENTITY_ID = 3
+
+
+def create_label(name, uri, width, height, image_id):
+    roi = RoiI()
+    roi.setName(rstring(name))
+    mask = create_mask(0, 0, width, height, name)
+    mask.details.externalInfo = create_external_info(
+        uri, NGFF_LABEL_ENTITY_TYPE, NGFF_LABEL_ENTITY_ID)
+    roi.addShape(mask)
+    roi.setImage(ImageI(image_id, False))
+    return roi
+
+
+def create_mask(x, y, width, height, text_value, color=[255, 255, 0, 100]):
+    mask = MaskI()
+    mask.setX(rdouble(x))
+    mask.setY(rdouble(y))
+    mask.setWidth(rdouble(width))
+    mask.setHeight(rdouble(height))
+    mask.setTextValue(rstring(text_value))
+    fill_color = int.from_bytes(color, byteorder="big", signed=True)
+    mask.setFillColor(rint(fill_color))
+    return mask
+
+
+def create_external_info(uri, entity_type, entity_id):
+    externalInfo = ExternalInfoI()
+    externalInfo.entityType = rstring(entity_type)
+    externalInfo.entityId = rlong(entity_id)
+    externalInfo.lsid = rstring(uri)
+    return externalInfo
+
+
+def query_pixels(conn, image_id):
+    params = omero.sys.ParametersI()
+    params.addId(image_id)
+    query = "select p from Pixels p where p.image.id = :id"
+    return conn.getQueryService().findByQuery(
+        query, params, {"omero.group": "-1"})
+
+
+def query_mask(conn, roi_id):
+    params = omero.sys.ParametersI()
+    params.addId(roi_id)
+    query = (
+        "select m from Mask m "
+        "join fetch m.details.externalInfo "
+        "where m.roi.id = :id")
+    return conn.getQueryService().findByQuery(
+        query, params, {"omero.group": "-1"})
